37a39a021322e5219f446ae15a3c674b46898be7
saeedmohamed <saeedmohamed@techytypes.com>
2024-11-03 09:55:12 +0200

    wip:Implement tasks dashboard ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,49 @@
-import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      this my first page
+    <div className="flex flex-col gap-5 p-10">
+      <div className="text-2xl font-semibold">Tasks Dashboard</div>
+      <div className="flex gap-10">
+        <Card className="w-[400px] min-h-[700px] ">
+          <CardHeader className="flex flex-row gap-2 items-center">
+            <div className="w-6 h-6 bg-slate-700 rounded-sm"></div>
+            <CardTitle className="!mt-0 text-xl">Todo</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-3">
+            <Card className="cursor-pointer hover:bg-gray-100">
+              <CardHeader>
+                <CardTitle className="!mt-0">Create project</CardTitle>
+                <CardDescription>
+                  Create a new project to start adding tasks to.
+                </CardDescription>
+              </CardHeader>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle className="!mt-0">Create project</CardTitle>
+                <CardDescription>
+                  Create a new project to start adding tasks to.
+                </CardDescription>
+              </CardHeader>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle className="!mt-0">Create project</CardTitle>
+                <CardDescription>
+                  Create a new project to start adding tasks to.
+                </CardDescription>
+              </CardHeader>
+            </Card>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
